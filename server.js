419f50ee39b5d78c1efef0f0f6bf549a04d5fc11
Erik Kuiper <eer.kuiper@avans.nl>
2018-04-25 14:42:57 +0200

    error

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express')
 var app = express()
  
+app.use('*', (req, res, next) => {
+  next()
+})
+
 app.get('/', function (req, res) {
   res.send('Hallo allemaal')
 })
- 
+
+app.use('*', (err, req, res, next) => {
+  console.log(err)
+})
+
 app.listen(3000)
