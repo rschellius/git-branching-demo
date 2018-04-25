3eaa1e569b1c7c9ff563c4b5c0c6658aab30f3a2
Thomas De Lange <thomas.m.delange@gmail.com>
2018-04-25 14:44:50 +0200

    SwaggyBranch versie 1.1
    Updated
    Hallo allemaal op andere url

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
+app.get('/ANK', function (req, res) {
  res.send('HALLO ALLEMAAAL, WAT FIJN DAT JE ER BENT. BEN JE VOOR HET EERST HIEEEEER...? OF BEN JE AL BEKEND? STAMP MET JE VOETEN *STAMP*, ZET JE HANDEN IN JE ZEI. IK BEN ANK EN WIE BEN JIIIIIJ')
 
 
